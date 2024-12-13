54bdcd86ccafccc11f03f6338a69531a467b2b17
Surendra <surendravikram85@gmail.com>
2024-12-14 01:32:20 +0530

    reverse the array using custom method

---
+++ reverseArray.js
@@ -0,0 +1,17 @@
+console.log("Array");
+
+let arr = [10, 2, 3, 9, 5, 6, 7, 8];
+
+function reverseArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      let temp = arr[j];
+      arr[j] = arr[arr.length - 1];
+      arr[arr.length - 1] = temp;
+    }
+  }
+  return arr;
+}
+
+let res = reverseArray(arr);
+console.log(res);
